d49966654bd01fdae8c711f352d7fc3f97b34570
Daniel Thompson <dan@d4software.co.uk>
2024-06-09 17:22:26 +0100

    Adding some nock tests

--- tests/file.test.js
+++ tests/file.test.js
@@ -1,4 +1,5 @@
-const { SQLizerFile } = require("../lib/file.js");
+const { SQLizerFile } = require("../lib/");
+const nock = require("nock");
 
 const defaultParams = {
   FileType: "csv",
@@ -9,7 +10,7 @@ const defaultParams = {
   Delimiter: ",",
   CheckTableExists: true,
   InsertSpacing: 250,
-  Path: "./test1.csv",
+  Path: "./tests/test1.csv",
 };
 
 describe("SQLizerFile", () => {
@@ -43,5 +44,139 @@ describe("SQLizerFile", () => {
       testRequiredParameter("DatabaseType"));
     it("should throw if Path is not passed", () =>
       testRequiredParameter("Path"));
+  });
+
+  describe("convert", () => {
+    const fileId = "12345";
+
+    nock("https://sqlizer.io")
+      .post("/api/files")
+      .reply(200, {
+        ID: fileId,
+        Status: "New",
+      })
+      .post(`/api/files/${fileId}/data`)
+      .reply(200, {})
+      .get(`/downloads/${fileId}`)
+      .reply(200, "CREATE TABLE my_table...")
+      .persist();
+
+    it("should save the fields from the API", async () => {
+      const responseObject = {
+        ID: "12345",
+        Status: "Complete",
+        ResultUrl: `https://sqlizer.io/downloads/${fileId}`,
+      };
+
+      nock("https://sqlizer.io")
+        .put(`/api/files/${fileId}`)
+        .reply(200, responseObject);
+
+      const file = new SQLizerFile(defaultParams);
+      await file.convert();
+      expect(file.ID).toBe(fileId);
+      expect(file.Status).toBe(responseObject.Status);
+    });
+
+    it("should keep calling the API until the Status reaches Complete", async () => {
+      nock("https://sqlizer.io").put(`/api/files/${fileId}`).reply(200, {
+        ID: "12345",
+        Status: "Queued",
+      });
+
+      const scope = nock("https://sqlizer.io")
+        .get(`/api/files/${fileId}`)
+        .reply(200, {
+          ID: "12345",
+          Status: "Analysing",
+        })
+        .get(`/api/files/${fileId}`)
+        .reply(200, {
+          ID: "12345",
+          Status: "Processing",
+        })
+        .get(`/api/files/${fileId}`)
+        .reply(200, {
+          ID: "12345",
+          Status: "Complete",
+          ResultUrl: `https://sqlizer.io/downloads/${fileId}`,
+        });
+
+      const file = new SQLizerFile(defaultParams);
+      await file.convert();
+      expect(file.ID).toBe(fileId);
+      expect(file.Status).toBe("Complete");
+      expect(scope.isDone()).toBe(true);
+    });
+
+    it("should time out if the timeout is reached", async () => {
+      nock("https://sqlizer.io").put(`/api/files/${fileId}`).reply(200, {
+        ID: "12345",
+        Status: "Queued",
+      });
+
+      const scope = nock("https://sqlizer.io")
+        .get(`/api/files/${fileId}`)
+        .times(10)
+        .reply(200, {
+          ID: "12345",
+          Status: "Analysing",
+        });
+
+      const file = new SQLizerFile(defaultParams);
+      let error;
+      try {
+        await file.convert(3000); // 3 second timeout
+      } catch (err) {
+        error = err;
+      }
+      expect(error).toBeTruthy();
+      expect(error.message).toBe("Conversion timeout reached");
+      expect(file.ID).toBe(fileId);
+      expect(file.Status).toBe("Analysing");
+    });
+
+    it("should capture any error message from the initial upload", async () => {
+      nock("https://sqlizer.io").put(`/api/files/${fileId}`).reply(200, {
+        Status: "Failed",
+        Message: "That file is too big",
+      });
+
+      const file = new SQLizerFile(defaultParams);
+      let error;
+      try {
+        await file.convert();
+      } catch (err) {
+        error = err;
+      }
+      expect(error).toBeTruthy();
+      expect(file.Status).toBe("Failed");
+      expect(file.Message).toBe("That file is too big");
+    });
+
+    it("should capture any error message from during the conversion", async () => {
+      nock("https://sqlizer.io").put(`/api/files/${fileId}`).reply(200, {
+        ID: "12345",
+        Status: "Queued",
+      });
+
+      const scope = nock("https://sqlizer.io")
+        .get(`/api/files/${fileId}`)
+        .reply(200, {
+          Status: "Failed",
+          Message: "That file is incorrectly formatted",
+        });
+
+      const file = new SQLizerFile(defaultParams);
+      let error;
+      try {
+        await file.convert();
+      } catch (err) {
+        error = err;
+      }
+      expect(error).toBeTruthy();
+      expect(file.Status).toBe("Failed");
+      expect(file.Message).toBe("That file is incorrectly formatted");
+    });
   });
 });
